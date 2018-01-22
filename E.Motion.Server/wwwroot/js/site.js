2294e060f6397b9e1811fdb9cbe9913099b31e73
Thomas Pentenrieder <pentenrieder@hotmail.com>
2018-01-22 10:42:09 +0100

    videos

--- E.Motion.Server/wwwroot/js/site.js
+++ E.Motion.Server/wwwroot/js/site.js
@@ -5,16 +5,29 @@ $(function () {
     $("video").hide();
     function start() {
         console.log("starting");
-        //$("video").attr("src", "/videos/default.mp4");
+        $("video").removeAttr("loop");
+        $("video").attr("src", "/videos/neuer/intro.mp4");
         $("video").show();
+        $('video').on('ended', function () {
+            this.currentTime = 9;
+        });
     }
     function reset() {
+        $("video").off('ended');
         $("video").hide();
         console.log("resetting");
     }
     function emotion(e) {
         console.log(e);
-        $("video").attr("src", "/videos/" + e + ".mp4");
+        if (e == "sadness") {
+            e = "anger";
+        }
+        if (e == "neutral") {
+            e = "hapiness";
+        }
+        $("video").off('ended');
+        $("video").attr("src", "/videos/neuer/" + e + ".mp4");
+        $("video").attr("loop", true);
         $("video")[0].load();
     }
     let connection = new signalR.HubConnection('/updater');
